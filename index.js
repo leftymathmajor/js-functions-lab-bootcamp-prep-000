b48283aae453c854166bf2cabd935be7028257c6
Kristine Wise <leftymathmajor@yahoo.com>
2018-02-03 04:41:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 // write your code below!
 function happyholidays(){return "Happy Holidays"}
 function happyHolidaysTo(name){return 'Happy Holidays, ${name}!'}
-function happy
+function happyHolidaysTo(holiday, name){return 'Happy ${holiday}, ${name}!'}
+function holidayCountdown(holiday, days){return 'Its ${days} days until ${holiday}!'}
