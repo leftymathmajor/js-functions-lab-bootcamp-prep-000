f4827df72b00d717a4fbd4a8eec08996e87d4a93
Kristine Wise <leftymathmajor@yahoo.com>
2018-02-03 04:50:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // write your code below!
-function happyholidays(){return "Happy Holidays"}
-function happyHolidaysTo(name){return "Happy Holidays, ${name}!"}
-function happyHolidaysTo(holiday, name){return "Happy ${holiday}, ${name}!"}
-function holidayCountdown(days, holiday){ return "It's ${days} days until ${holiday}!"}
+function happyHolidays() {return "Happy holidays!";}
+function happyHolidaysTo(name) {return `Happy holidays, ${name}!`;}
+function happyHolidayTo(holiday, name) {return `Happy ${holiday}, ${name}!`}
+function holidayCountdown(holiday, days) {return `It's ${days} days until ${holiday}!`;} 
